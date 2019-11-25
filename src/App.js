3ab4f3061bfecccea248f5edaa9bc9c789439151
Casweeney <casweeno2000@gmail.com>
2019-11-25 16:54:49 +0100

    completed todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import AddTodo from './AddTodo';
+import TodoList from './TodoList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = 'http://localhost:8000/api/todos';
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      todos: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get(API_URL)
+      .then((response) => {
+        this.setState({todos: response.data.todos});
+      });
+  }
+
+  deleteTodo = (todoId) => {
+    axios.post(`${API_URL}/delete`,{id: todoId})
+      .then(() => {
+        const todos = this.state.todos.filter((todo) => todo.id !==  todoId);
+
+        this.setState({todos});
+      })
+  }
+  
+  completeTodo = (todoId) => {
+    axios.post(`${API_URL}/complete`,{id: todoId})
+      .then(() => {
+        const todos = this.state.todos.map((todo) => {
+          if(todo.id == todoId){
+            todo.completed = !todo.completed;
+          }
+
+          return todo;
+        });
+
+        this.setState({todos});
+      })
+  }
+
+  createTodo = (content) => {
+    axios.post(API_URL, {content})
+      .then((response) => {
+        const todos = this.state.todos.concat([response.data.todo]);
+
+        this.setState({todos});
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddTodo createTodo={this.createTodo} />
+        <TodoList todos={this.state.todos} 
+          completeTodo={this.completeTodo} 
+          deleteTodo={this.deleteTodo} 
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
